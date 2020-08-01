3147895b64b3204fd9fe95a7c32096ea1e194758
lockcy <755718761@qq.com>
2020-08-01 13:24:06 +0800

    Site updated: 2020-08-01 13:24:03

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 //const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "http://localhost:5000/live2d-widget/";
+const live2d_path = "https://lockcy.github.io/live2d-widget/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
