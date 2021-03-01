c5c31bb6a6aa114763f42ea0e851d99c9a156f34
lockcy <755718761@qq.com>
2021-03-01 19:45:28 +0800

    Site updated: 2021-03-01 19:45:09

---
+++ dist/music.js
@@ -0,0 +1,51 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    autoplay: false,
+    loop: 'all',
+    volume: 0.3,
+    listFolded: false,
+    listMaxHeight: 60,
+    mutex: true,
+    preload: 'auto',
+    audio: [
+        {
+            name: '瞬き',
+            artist: '春茶オサム',
+            url: 'http://music.163.com/song/media/outer/url?id=556042458.mp3',
+            cover: 'http://p1.music.126.net/O_FQeMbLFE39rsW4glCr7Q==/109951164061739945.jpg?param=130y130',
+        },
+        {
+            name: '打上花火',
+            artist: 'kobasolo / 春茶 / 伊礼亮',
+            url: 'http://music.163.com/song/media/outer/url?id=1338972497.mp3',
+            cover: 'http://p2.music.126.net/ErIn1C-8bLhROh0-wyljaw==/109951163894382465.jpg?param=130y130',
+        },
+        {
+            name: 'Lemon',
+            artist: 'kobasolo / 春茶',
+            url: 'http://music.163.com/song/media/outer/url?id=1339044717.mp3',
+            cover: 'http://p2.music.126.net/ErIn1C-8bLhROh0-wyljaw==/109951163894382465.jpg?param=130y130',
+        },
+        {
+            name: '恋音と雨空',
+            artist: '春茶',
+            url: 'http://music.163.com/song/media/outer/url?id=1420072252.mp3',
+            cover: 'http://p1.music.126.net/YydedMxFaskj7txCCekDCw==/109951164666603036.jpg?param=130y130',
+        },
+        {
+            name: '貴方解剖純愛歌〜死ね〜',
+            artist: '春茶',
+            url: 'http://music.163.com/song/media/outer/url?id=1351268113.mp3',
+            cover: 'http://p1.music.126.net/YydedMxFaskj7txCCekDCw==/109951164666603036.jpg?param=130y130',
+        },
+        {
+            name: '灰色と青',
+            artist: 'kobasolo / 春茶',
+            url: 'http://music.163.com/song/media/outer/url?id=1339048598.mp3',
+            cover: 'http://p1.music.126.net/YydedMxFaskj7txCCekDCw==/109951164666603036.jpg?param=130y130',
+        },
+
+    ]
+});
+
+
